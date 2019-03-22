a6dae570d94f1de755b36d97e02449d298151e3c
SDPOPT <houke21@gmail.com>
2019-03-22 17:13:54 +0800

    2019/3/22

--- AT1_model.R
+++ AT1_model.R
@@ -14,7 +14,8 @@ blpConnect()
 
 ID <- tibble( ID = c("GB00BQY78G05 Index", "GB00BQY78F97 Index", 
                      "EU0009658426 Index", "US06746L4225 US Equity",
-                     "USGG10YR  Index", "DXY Curncy")) %>%
+                     "VGIT US Equity", "DXY Curncy", "SPX Index",
+                     "IYG3X Index", "USGG10YR  Index")) %>%
   mutate(ticker = bdp(ID, "TICKER")$TICKER)
 
 Price <- bdh(ID$ID, "PX_LAST", Sys.Date() - 3000, Sys.Date() - 1) 
@@ -32,27 +33,46 @@ Price1 <- Price %>%
   ungroup() %>%
   filter(count == max(count)) %>%
   select(date, ticker, price = PX_LAST) %>%
-  spread(ticker, price)
+  group_by(ticker) %>%
+  arrange(-desc(date)) %>%
+  mutate(return = price / lag(price) - 1,
+         return = ifelse(is.na(return) == TRUE, 0, return)) %>%
+  ungroup() %>%
+  gather(price, return, key = "cat", value = "value")
   
+key <- "return"
 
 train <-Price1 %>%
+  filter(cat == key) %>%
   arrange(-desc(date)) %>%
-  filter(row_number(date) <= 1000)
+  group_by(ticker) %>%
+  ungroup() %>%
+  spread(ticker, value) %>%
+  filter(row_number(date) <= 500)
 
 test <- Price1 %>%
+  filter(cat == key) %>%
   arrange(-desc(date)) %>%
-  filter(row_number(date) > 1000)
+  group_by(ticker) %>%
+  ungroup() %>%
+  spread(ticker, value) %>%
+  filter(row_number(date) > 500)
 
-at1_model <- lm(IBXXC1P1 ~ SX7E + VXX + USGG10YR, data = train)
+at1_model <- lm(IBXXC1P1 ~ DXY + SX7E + VXX, data = train)
 summary(at1_model)
 
 anova(at1_model)
 
-test <- test %>%
+result <- test %>%
   mutate(result = predict(at1_model, test))
 
-ggplot(data = test) +
-  # geom_line(aes(x = date, y = IBXXC1P1)) +
-  # geom_line(aes(x = date, y = result), color = "blue") +
-  geom_area(aes(x = date, y = result - IBXXC1P1))
+return <- result %>%
+  select(date, IBXXC1D1, result) %>%
+  mutate(index_return = percent(IBXXC1D1 / IBXXC1D1[1] - 1),
+         model_return = percent(result / result[1] - 1),
+         strategy = percent(index_return - model_return))
 
+ggplot(data = return) +
+  geom_line(aes(x = date, y = index_return)) +
+  geom_line(aes(x = date, y = model_return), color = "red") +
+  geom_line(aes(x = date, y = strategy), color = "blue")
